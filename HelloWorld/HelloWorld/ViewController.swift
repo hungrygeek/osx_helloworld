45f6908dbc9d00578677ee38d57781c673dbf37f
Shi Shu <s.shu@wustl.edu>
2016-07-07 09:08:08 -0500

    Initial Commit
    OSX Dev tutorial

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by ShiShu on 6/16/16.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
